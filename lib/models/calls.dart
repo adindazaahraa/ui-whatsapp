3c3e3ee955f8b7da2d1b0414d77c4f7577d58f85
adindazahrs <zheeerr21@gmail.com>
2024-03-12 10:03:30 +0700

    first commit

---
+++ lib/models/calls.dart
@@ -0,0 +1,14 @@
+class Status {
+  String name;
+  String statusDate;
+  Status({required this.name, required this.statusDate});
+}
+var callsList = [
+  Status(name: "Maya", statusDate: "Now, 13:00 PM"),
+  Status(name: "Syitha", statusDate: "Now, 13:00 PM"),
+  Status(name: "Mama", statusDate: "Now, 13:00 PM"),
+  Status(name: "Syitha", statusDate: "Now, 13:00 PM"),
+  Status(name: "Maya", statusDate: "Now, 13:00 PM"),
+  Status(name: "Maya", statusDate: "Now, 13:00 PM"),
+  Status(name: "Syitha", statusDate: "Now, 13:00 PM"),
+];
---
+++ lib/models/status.dart
@@ -0,0 +1,11 @@
+class Status {
+  String name;
+  String statusDate;
+
+  Status({required this.name, required this.statusDate});
+}
+
+var statusList = [
+  Status(name: "Pak Ridho", statusDate: "Now, 18:00 PM"),
+  Status(name: "Mas Dzul", statusDate: "Now, 21:00 PM"),
+];
